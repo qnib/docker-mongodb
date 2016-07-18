c82bf893f622ee0c5b80aad907bde006f8d6682e
Christian Kniep <christian@qnib.org>
2016-07-18 11:16:20 +0200

    enhance restore script

--- opt/qnib/mongodb/bin/restore.sh
+++ opt/qnib/mongodb/bin/restore.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 if [ "X${MONGO_RESTORE}" != "X" ];then
-   if [ -f ${MONGO_RESTORE} ];then
+   if [ "${MONGODB_TARGET}X" != "X" ] && [ -f "${MONGODB_DIR}/${MONGODB_TARGET}" ];then
+      echo "> Skip restore, as the target '${MONGODB_DIR}/${MONGODB_TARGET}' is already there..."
+      sleep 5
+      exit 0
+   elif [ -f ${MONGO_RESTORE} ];then
       tar xf ${MONGO_RESTORE} -C /tmp/
       mongorestore /tmp/dump/
    else
       echo "Sorry, the file '${MONGO_RESTORE}' does not exist"
+      sleep 5
    fi
 fi
